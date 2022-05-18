a8aed9a5997a3ec687158f30e5df25fdd4e76dac
Gabriel <gopxfs@hotmail.com>
2022-05-18 14:49:02 -0300

    Finish featured stars section

--- script.js
+++ script.js
@@ -9,16 +9,56 @@ const starName = [];
 const starAbout = [];
 const starLine = [];
 const starDescription = [];
+featuredStars.innerHTML = 'Featured Stars';
 // Adding id/classes
-starAbout.setAttribute('id', 'star-about');
-starDescription.setAttribute('id', 'star-description');
+starAbout.id = 'star-about';
+starDescription.id = 'star-description';
 orangeLine.classList.add('orange-line');
-starLine.classList.add('grey-line');
 // Appending children
 featuredStars.appendChild(featuredTitle);
 featuredStars.appendChild(orangeLine);
-// Creating and appending divs and its elements i times
+// Creating an array of objects to store stars data
+const starData = [
+  {
+    name: '1',
+    image: '',
+    about: 'a1',
+    description: 'd1',
+  },
+  {
+    name: '2',
+    image: '',
+    about: 'a2',
+    description: 'd2',
+  },
+  {
+    name: '3',
+    image: '',
+    about: 'a3',
+    description: 'd3',
+  },
+  {
+    name: '4',
+    image: '',
+    about: 'a4',
+    description: 'd4',
+  },
+  {
+    name: '5',
+    image: '',
+    about: 'a5',
+    description: 'd5',
+  },
+  {
+    name: '6',
+    image: '',
+    about: 'a6',
+    description: 'd6',
+  },
+];
+// A for loop to set star elements i times
 for (let i = 0; i < 6; i += 1) {
+  // Creating
   starDiv[i] = document.createElement('div');
   starImage[i] = document.createElement('img');
   starInformation[i] = document.createElement('div');
@@ -26,11 +66,18 @@ for (let i = 0; i < 6; i += 1) {
   starAbout[i] = document.createElement('p');
   starLine[i] = document.createElement('span');
   starDescription[i] = document.createElement('p');
+  starLine[i].classList.add('grey-line');
+  // Appending
   featuredStars.appendChild(starDiv[i]);
   starDiv[i].appendChild(starImage[i]);
-  starDiv[i].appendChild(starInformation);
+  starDiv[i].appendChild(starInformation[i]);
   starInformation[i].appendChild(starName[i]); 
   starInformation[i].appendChild(starAbout[i]); 
   starInformation[i].appendChild(starLine[i]); 
-  starInformation[i].appendChild(starDescription[i]); 
+  starInformation[i].appendChild(starDescription[i]);
+  // Assigning data
+  starImage[i].src = starData[i].image;
+  starName[i].innerHTML = starData[i].name;
+  starAbout[i].innerHTML = starData[i].about;
+  starDescription[i].innerHTML = starData[i].description;
   }
